c2b52cd5eb08784b1d879f4dd05391d83f7c9886
ikunkk02-afk <ikunkk02@gmail.com>
2026-07-31 13:59:17 +0800

    chore: backup initial Fabric 1.21.1 template

---
+++ src/client/java/io/github/ikunkk02afk/workshopzone/client/Workshop_zoneClient.java
@@ -0,0 +1,10 @@
+package io.github.ikunkk02afk.workshopzone.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Workshop_zoneClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/io/github/ikunkk02afk/workshopzone/client/Workshop_zoneDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.ikunkk02afk.workshopzone.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Workshop_zoneDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
